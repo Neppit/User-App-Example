825b7ee03a5844e49a73f5cb79e4869d6c014c64
Fluttershy629 <aliceayres956@gmail.com>
2024-04-09 11:23:55 -0400

    inquirer

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-const { print, add } = require('./lib/studentTools.js')
-const dayjs = require('dayjs')
+const inquirer = require('inquirer')
 
-const studentName = process.argv[2]
+inquirer.prompt([{
+    type: 'input',
+    name: 'color',
+    message: 'Please type your favorite color'
+},
 
+{
+    type: 'confirm',
+    name: 'likesNode',
+    message: 'Do you like Node JS?'
+},
 
-if (studentName === 'print') {
-    print()
-} else {
-    const date = dayjs().format('MM/DD/YYYY')
-
-    add(studentName + ' - ' + date)
-}
-
+{
+    type: 'list',
+    name: 'answer',
+    message: 'What do we use to import a package into a JS file in Node?',
+    choices: ['return', 'require', 'function'],
+},
+{
+    type: 'checkbox',
+    name: 'food',
+    message: 'What is your favorite food genre?',
+    choices: ['Korean', 'Turkish'],
+}]
+)
+    .then((answerObj) => {
+        console.log(answerObj)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
