af2f1210ee4015b1d6488c16bd9fd6b83b009bb6
Fluttershy629 <aliceayres956@gmail.com>
2024-04-11 10:10:05 -0400

    oop-basics

--- index.js
+++ index.js
@@ -1,28 +1,89 @@
-// const alice = {
-//     name: 'Alice',
-//     age: 23
+
+class Phone {
+    screen = true
+
+    constructor(number, size, model, color) {
+        this.number = number
+        this.size = size
+        this.model = model
+        this.color = color
+    }
+}
+
+class iPhone extends Phone {
+    facetime = true
+
+    constructor(number, size, model, color, appleID) {
+        super(number, size, model, color)
+        this.appleID = appleID
+    }
+}
+
+class Samsung extends Phone{
+    foldable = true
+
+    constructor(number, size, model, color, repairID) {
+        super(number, size, model, color)
+        this.repairID = repairID
+    }
+}
+
+const alicePhone = new iPhone('555-555-5555', 'standard', '15', 'slate grey', 'sdajdjsadjsad')
+const benPhone = new Samsung('555-555-5555', 'standard', '15', 'slate grey', 'sdajdjsadjsad')
+
+console.log(alicePhone)
+console.log(benPhone)
+
+// class Person {
+//     species = 'homosapien'
+
+//     constructor(name, age, hobbies) {
+//         this.name = name
+//         this.age = age
+//         this.hobbies = hobbies
+//     }
+
+//     haveBirthday() {
+//         this.age++
+//         console.log('Happy Birthday', this.name)
+//     }
+
+//     printBirthday() {
+//         console.log(`${this.name} is ${this.age} years old.`)
+//     }
+//     printHobbies() {
+//         console.log('\nHobbies\n--------')
+
+//         this.hobbies.forEach(hobby => {
+//             console.log(hobby)
+//         })
+//     }
 // }
 
-function Person(name, age, hobbies) {
-    this.name = name
-    this.age = age
-    this.hobbies = hobbies
-    }
+// const alice = new Person('Alice', '23', ['writing', 'sleep'])
 
-    Person.prototype.haveBirthday = function() {
-        this.age++
-        console.log('Happy Birthday')
-    }
+// console.log(alice)
 
-    Person.prototype.printBirthday = function() {
-        console.log(`You are ${this.age} years old.`)
-    }
+// function Person(name, age, hobbies) {
+//     this.name = name
+//     this.age = age
+//     this.hobbies = hobbies
+//     }
 
-    Person.prototype.species = 'homosapien'
+//     Person.prototype.haveBirthday = function() {
+//         this.age++
+//         console.log('Happy Birthday')
+//     }
 
-const alice = new Person('Alice', '23', ['writing', 'sleeping'])
-const bob = new Person('Bob', '40', ['Bingo', 'Sitting'])
+//     Person.prototype.printBirthday = function() {
+//         console.log(`You are ${this.age} years old.`)
+//     }
 
-alice.haveBirthday()
-alice.printBirthday()
-console.log(alice)
+//     Person.prototype.species = 'homosapien'
+
+// const alice = new Person('Alice', '23', ['writing', 'sleeping'])
+// const bob = new Person('Bob', '40', ['Bingo', 'Sitting'])
+
+// alice.haveBirthday()
+// alice.printBirthday()
+// console.log(alice)
