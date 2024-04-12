eca51118896b092b45c40c4ef6ccfa94f3d186b9
Fluttershy629 <aliceayres956@gmail.com>
2024-04-12 12:22:45 -0400

    starwars

--- index.js
+++ index.js
@@ -1,35 +1,53 @@
-// class Prom {
-//     then (cb) {
-//         setTimeout(() => {
-//         cb() 
-//         }, 3000
-//     )
-//     }
-//     catch (cb) {
+const axios = require('axios')
+const inquirer = require('inquirer')
 
-//     }
-// }
+const baseURL = 'https://swapi.dev/api'
 
-// const prom = new Prom()
+function outputSearchResults(data) {
+    console.log('\nResults:\n---------')
 
-// prom.then(() => {
-//     console.log('callback called')
-// })
-
-function handlesSomeTask(isCool) {
-    return new Promise((resolve, reject) => {
-        if (isCool) {
-            resolve()
-        } else {
-            reject()
-        }
+    data.results.forEach(result => {
+        console.log('Name:', result.name)
     })
 }
 
-handlesSomeTask()
-    .then(() => {
-        console.log('all good')
+async function makeRequest(data) {
+    const url = `${baseURL}/${data.dataset.toLowerCase()}?search=${data.search}`
+
+    const res = await axios.get(url)
+    
+    return res.data
+}
+
+async function getSearch(datasetObject) {
+    const searchObject = await inquirer.prompt({
+        name: 'search',
+        message: 'Please type a search word for your chosen dataset'
     })
-    .catch(() => {
-        console.log('all bad')
+
+    return {
+        datasetObject: datasetObject,
+        searchObject: searchObject,
+    }
+}
+
+async function getDataset() {
+    const answerObj = await inquirer.prompt({
+        name: 'choice',
+        type: 'list',
+        message: 'Please select a dataset from the list',
+        choices: ['Films', 'People', 'Planets', 'Species', 'Starships', 'Vehicles']
     })
+
+    return answerObj
+}
+
+getDataset()
+    .then(getSearch)
+    .then(makeRequest)
+    .then(outputSearchResults)
+
+// axios.get(baseURL + '/planets')
+//     .then((res) => {
+//         console.log(res.data)
+//     })
